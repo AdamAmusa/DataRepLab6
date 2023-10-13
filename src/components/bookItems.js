ffdbcb6fab62071fdb19ac4fe7985ed4caad92ba
Adam Amusa <Adamamusa2004@gmail.com>
2023-10-13 14:35:45 +0000

    lab3 complete

--- src/components/bookItems.js
+++ src/components/bookItems.js
@@ -6,14 +6,14 @@ function Bookitems(props) {
 
 
         <div>
-
+            {/*Card container */}
             <Card>
-                <Card.Header>{props.myBook.title}</Card.Header>
+                <Card.Header>{props.myBook.title}</Card.Header> {/*Prints title of books */}
                 <Card.Body>
                 <blockquote className = "blockquote mb-0">   
-                    <img src={props.myBook.thumbnailUrl}></img>
+                    <img src={props.myBook.thumbnailUrl}></img>{/*Prints out photo of books */}
                     <footer>
-                    <Card.Text>{props.myBook.authors[0]}</Card.Text>
+                    <Card.Text>{props.myBook.authors[0]}</Card.Text>{/*Prints out authors of books */}
                     </footer>
                     </blockquote> 
                 </Card.Body>
--- src/components/books.js
+++ src/components/books.js
@@ -2,9 +2,10 @@ import Bookitems from "./bookItems";
 
 function Books(props) {
 
+    //splits the array of books into individual books
     return props.myBook.map(
         (book) => {
-            return <Bookitems myBook={book} key={book.isbn}></Bookitems>
+            return <Bookitems myBook={book} key={book.isbn}></Bookitems> //passes data to book item component
 
         }
     );
